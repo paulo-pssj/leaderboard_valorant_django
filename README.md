b9e170d77506f371a9089648f807afa06c351238
paulo-pssj <paulinhops99@gmail.com>
2022-03-04 15:27:08 -0300

    Arquivos do projeto

---
+++ README.md
@@ -0,0 +1,6 @@
+# Leaderboard Valorant
+Neste projeto criei um leaderboard com os dados da API do Valorant que a Riot Games disponibiliza. <br/>
+
+Utilizei Python e o Framework Django
+
+API para pegar informações do Ranking dos Top Tiers do Valorant Br: https://developer.riotgames.com/apis#val-ranked-v1/GET_getLeaderboard
---
+++ ranking/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import requests
+
+def index(request):
+
+    url = 'https://br.api.riotgames.com/val/ranked/v1/leaderboards/by-act/SUA API KEY?size=200&startIndex=0&api_key=RGAPI-76a5d312-0769-47f8-bb62-15ef5d893473'
+
+    data_full = requests.get(url).json()
+
+    data_players = []
+
+    for player in data_full['players']:
+        if 'gameName' in player:
+            data = {
+                'nickname': player['gameName'],
+                'rank': player['leaderboardRank'],
+                'points': player['rankedRating'],
+                'wins': player['numberOfWins']
+            }
+            data_players.append(data)
+        else:
+            data = {
+                'nickname': 'Secreto',
+                'rank': player['leaderboardRank'],
+                'points': player['rankedRating'],
+                'wins': player['numberOfWins']
+            }
+            data_players.append(data)
+
+    context = {'data_players': data_players}
+
+    return render(request, 'ranking/index.html', context)
